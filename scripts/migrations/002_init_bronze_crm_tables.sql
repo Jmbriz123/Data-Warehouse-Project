82902b436a53e1c3dce9ee4c1b6b2c057b9120f4
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-01 12:08:39 +0800

    refactor(bronze): use TEXT data types instead of VARCHAR(n) for flexibility to avoid ingestion failures

--- scripts/migrations/002_init_bronze_crm_tables.sql
+++ scripts/migrations/002_init_bronze_crm_tables.sql
@@ -6,29 +6,29 @@ DROP TABLE IF EXISTS bronze.crm_sales_details CASCADE;
 
 CREATE TABLE IF NOT EXISTS bronze.crm_cust_info(
     cst_id INTEGER,
-    cst_key VARCHAR(50),
-    cst_firstname VARCHAR(50),
-    cst_lastname VARCHAR(50),
-    cst_marital_status VARCHAR(50),
-    cst_gndr VARCHAR(50),
+    cst_key TEXT,
+    cst_firstname TEXT,
+    cst_lastname TEXT,
+    cst_marital_status TEXT,
+    cst_gndr TEXT,
     cst_create_date DATE
 );
 
 
 CREATE TABLE IF NOT EXISTS bronze.crm_prd_info(
     prd_id INTEGER,
-    prd_key VARCHAR(50),
-    prd_nm VARCHAR(50),
+    prd_key TEXT,
+    prd_nm TEXT,
     prd_cost INTEGER,
-    prd_line VARCHAR(50),
+    prd_line TEXT,
     prd_start_dt DATE,
     prd_end_dt DATE
 );
 
 
 CREATE TABLE IF NOT EXISTS bronze.crm_sales_details(
-    sls_ord_num VARCHAR(50),
-    sls_prd_key VARCHAR(50),
+    sls_ord_num TEXT,
+    sls_prd_key TEXT,
     sls_cust_id INTEGER,
     sls_order_dt INTEGER,
     sls_ship_dt INTEGER,
