f26a16d71cb32c3c44faa6b4e2fc2caf1e5a63de
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-03 22:45:43 +0800

    feat(silver): initialize silver erp tables

---
+++ scripts/migrations/005_init_silver_erp_tables.sql
@@ -0,0 +1,33 @@
+-- Drop first to ensure the schema matches the code perfectly
+DROP TABLE IF EXISTS silver.erp_cust_az12 CASCADE;
+DROP TABLE IF EXISTS silver.erp_loc_a101 CASCADE;
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2 CASCADE;
+
+
+CREATE TABLE IF NOT EXISTS silver.erp_cust_az12(
+    cid TEXT,
+    bdate DATE,
+    gen TEXT,
+
+    -- Audit metadata (not from source CSV, added by pipeline)
+    _loaded_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS silver.erp_loc_a101(
+    cid TEXT,
+    cntry TEXT,
+
+    -- Audit metadata (not from source CSV, added by pipeline)
+    _loaded_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS silver.erp_px_cat_g1v2(
+    id TEXT,
+    cat TEXT,
+    subcat TEXT,
+    maintenance TEXT,
+
+    -- Audit metadata (not from source CSV, added by pipeline)
+    _loaded_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
