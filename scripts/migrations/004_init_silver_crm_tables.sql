11a21fc9db1cce52370ba0c9dd257cac2bff5fe0
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-03 22:42:15 +0800

    feat(silver): initialize silver crm tables

---
+++ scripts/migrations/004_init_silver_crm_tables.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS silver.crm_cust_info CASCADE;
+DROP TABLE IF EXISTS silver.crm_prd_info CASCADE;
+DROP TABLE IF EXISTS silver.crm_sales_details CASCADE;
+
+
+
+CREATE TABLE IF NOT EXISTS silver.crm_cust_info(
+    cst_id INTEGER,
+    cst_key TEXT,
+    cst_firstname TEXT,
+    cst_lastname TEXT,
+    cst_marital_status TEXT,
+    cst_gndr TEXT,
+    cst_create_date DATE,
+
+    -- Audit metadata (not from source CSV, added by pipeline)
+    _loaded_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS silver.crm_prd_info(
+    prd_id INTEGER,
+    prd_key TEXT,
+    prd_nm TEXT,
+    prd_cost INTEGER,
+    prd_line TEXT,
+    prd_start_dt DATE,
+    prd_end_dt DATE,
+
+    -- Audit metadata (not from source CSV, added by pipeline)
+    _loaded_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS silver.crm_sales_details(
+    sls_ord_num TEXT,
+    sls_prd_key TEXT,
+    sls_cust_id INTEGER,
+    sls_order_dt INTEGER,
+    sls_ship_dt INTEGER,
+    sls_due_dt  INTEGER,
+    sls_sales INTEGER,
+    sls_quantity INTEGER,
+    sls_price INTEGER,
+
+    -- Audit metadata (not from source CSV, added by pipeline)
+    _loaded_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
