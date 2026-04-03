d7b837b482f12583ed3d10688bf1e30f9e6df8b9
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-03 22:40:16 +0800

    refactor: add default values to audit metadata field on bronze table creations

--- scripts/migrations/003_init_bronze_erp_tables.sql
+++ scripts/migrations/003_init_bronze_erp_tables.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS bronze.erp_cust_az12(
     gen TEXT,
 
     -- Audit metadata (not from source CSV, added by pipeline)
-    _loaded_at          TIMESTAMP
+    _loaded_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS bronze.erp_loc_a101(
     cntry TEXT,
 
     -- Audit metadata (not from source CSV, added by pipeline)
-    _loaded_at          TIMESTAMP
+    _loaded_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS bronze.erp_px_cat_g1v2(
@@ -29,5 +29,5 @@ CREATE TABLE IF NOT EXISTS bronze.erp_px_cat_g1v2(
     maintenance TEXT,
 
     -- Audit metadata (not from source CSV, added by pipeline)
-    _loaded_at          TIMESTAMP
+    _loaded_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
