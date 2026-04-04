7290fd99081bf0f78729e3b09b09b5ab4183c690
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-05 01:24:45 +0800

    feat: implement query to generate a relation of transformed data from the crm_cust_info, solving the data quality issues found in crm_cust_info from the bronze layer

---
+++ scripts/silver/transform_and_load_crm_cust_info_data.sql
@@ -0,0 +1,27 @@
+
+--generate result set of data to be inserted in the silver.crm_cust_info
+
+SELECT cst_id,
+       cst_key,
+       TRIM(cst_firstname) AS cst_firstname, --remote unwanted spaces
+       TRIM(cst_lastname) AS cst_lastname, --remove unwanted spaces
+       CASE
+           WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+           WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Married'
+           ELSE 'n/a'
+       END AS cst_marital_status, --transform marital status into more readable and descriptive values
+       CASE
+           WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+           WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+           ELSE 'n/a'
+       END AS cst_gndr,
+       cst_create_date --no issues found, no need of transformation
+--generate a relation of rows after deduplication
+FROM (
+    SELECT *,
+        ROW_NUMBER() OVER(
+            PARTITION BY cst_id
+            ORDER BY cst_create_date DESC, _loaded_at DESC --tie-breaker
+            ) AS version_rank
+    FROM bronze.crm_cust_info) AS subquery
+WHERE version_rank = 1;
