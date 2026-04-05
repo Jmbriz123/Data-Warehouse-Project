2a5f4bda68bcf3559d73d46dbc930ab21c71536d
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-05 17:51:31 +0800

    feat: implement query with  applied data transformation from the bronze layer to prepare for load to silver layer

---
+++ scripts/silver/transform_and_load_crm_prd_info.sql
@@ -0,0 +1,19 @@
+--apply data transformations
+SELECT prd_id,
+       prd_key,
+       REPLACE(SUBSTRING(prd_key, 1,5), '-', '_') AS cat_id, --new derived column from prd_key. Replace - with _ for data format consistency across tables
+       SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key,
+       prd_nm,
+       (CASE
+           WHEN prd_cost IS NULL THEN 0
+           ELSE prd_cost END) AS prd_cost,
+       (CASE UPPER(TRIM(prd_line))
+           WHEN 'M' THEN 'Mountain'
+           WHEN  'R' THEN 'Road'
+           WHEN 'S' THEN 'Other Sales'
+           WHEN 'T' THEN 'Touring'
+           ELSE 'n/a' END
+           ) AS prd_line,
+       prd_start_dt,
+       LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt )  -1 AS prd_end_dt
+FROM bronze.crm_prd_info;
