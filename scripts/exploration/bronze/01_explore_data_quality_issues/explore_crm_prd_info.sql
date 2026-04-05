d3ec65a9816006f64ca94a982d2cc4553f6fd5ef
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-05 12:16:37 +0800

    feat: explore primary key violation on bronze.crm_prd_info (issue found)

---
+++ scripts/exploration/bronze/01_explore_data_quality_issues/explore_crm_prd_info.sql
@@ -0,0 +1,7 @@
+--Script Purpose: Exploration of data quality issues on bronze.crm_prd_info before transforming and loading the data into silver layer
+
+--1. Explore primary key violations: not unique and/or NULL
+SELECT COUNT(*) AS total_records,
+       COUNT(DISTINCT prd_key) AS total_unique_records,
+       COUNT(CASE WHEN prd_key IS NULL THEN 1 ELSE 0 END) AS total_null_records
+FROM bronze.crm_prd_info;
