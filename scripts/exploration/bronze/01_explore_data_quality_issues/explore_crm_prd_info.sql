bfa1de492b3d30de41540288d171bd8d5f874b2b
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-05 12:20:02 +0800

    fix: choose the right primary key on exploration query

--- scripts/exploration/bronze/01_explore_data_quality_issues/explore_crm_prd_info.sql
+++ scripts/exploration/bronze/01_explore_data_quality_issues/explore_crm_prd_info.sql
@@ -2,6 +2,6 @@
 
 --1. Explore primary key violations: not unique and/or NULL
 SELECT COUNT(*) AS total_records,
-       COUNT(DISTINCT prd_key) AS total_unique_records,
-       COUNT(CASE WHEN prd_key IS NULL THEN 1 ELSE 0 END) AS total_null_records
+       COUNT(DISTINCT prd_id) AS total_unique_records,
+       COUNT(CASE WHEN prd_id IS NULL THEN 1 ELSE 0 END) AS total_null_records
 FROM bronze.crm_prd_info;
