c5648b1ea5318b2856a08afa3d60266deb46dc69
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-05 12:37:09 +0800

    feat: explore prd_key value and compare the consistency of data formatting across different tables

--- scripts/exploration/bronze/01_explore_data_quality_issues/explore_crm_prd_info.sql
+++ scripts/exploration/bronze/01_explore_data_quality_issues/explore_crm_prd_info.sql
@@ -5,3 +5,12 @@ SELECT COUNT(*) AS total_records,
        COUNT(DISTINCT prd_id) AS total_unique_records,
        COUNT(CASE WHEN prd_id IS NULL THEN 1 ELSE 0 END) AS total_null_records
 FROM bronze.crm_prd_info;
+
+--2. Explore prd_key column values
+--Realization : prd_key substring is actually the product category, which is used as primary key on erp_px_cat_g1v2
+SELECT DISTINCT prd_key
+FROM bronze.crm_prd_info;
+--Explore if data format is consistent across tables
+--Realization: format is inconsistent. One uses _ as separator, the other one uses -
+SELECT id
+FROM bronze.erp_px_cat_g1v2;
