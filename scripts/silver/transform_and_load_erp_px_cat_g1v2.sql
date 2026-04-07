80ff67284e1754e759bf331ba471502df5f83347
Jemarco Briz <jemarcobriz123@gmail.com>
2026-04-07 16:01:38 +0800

    feat: extracct and load erp_px_cat_g1v2 from bronze to silver

---
+++ scripts/silver/transform_and_load_erp_px_cat_g1v2.sql
@@ -0,0 +1,17 @@
+--load extracted data into silver layer
+BEGIN;
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+INSERT INTO silver.erp_px_cat_g1v2 (id,
+                                    cat,
+                                    subcat,
+                                    maintenance)
+-- extract data (no need of transformation, data quality is already good from the source)
+SELECT
+    id,
+    cat,
+    subcat,
+    maintenance
+FROM bronze.erp_px_cat_g1v2;
+COMMIT;
+
+
